c9c3c32eba302ca1e4f3166d4cd88bfdd1934cfc
JAEWUNG LEE <james@jamesnet.dev>
2023-03-18 13:45:35 +0900

    Updates

---
+++ src/Kakao/Kakao/App.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Kakao
+{
+    internal class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Window window = new();
+            window.Show();
+        }
+    }
+}
---
+++ src/Kakao/Kakao/Starter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Kakao
+{
+    internal class Starter
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            _ = new App().Run();
+        }
+    }
+}
