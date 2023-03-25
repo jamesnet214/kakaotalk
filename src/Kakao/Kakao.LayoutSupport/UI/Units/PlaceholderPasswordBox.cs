f7b8751776eee01222f152f73fe4f4268dd85569
JAEWUNG LEE <james@jamesnet.dev>
2023-03-25 16:10:16 +0900

    Updates

---
+++ src/Kakao/Kakao.LayoutSupport/UI/Units/PlaceholderPasswordBox.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kakao.LayoutSupport.UI.Units
+{
+    public class PlaceholderPasswordBox : TextBox
+    {
+        public static readonly DependencyProperty PlaceholderTextProperty = DependencyProperty.Register(nameof(PlaceholderText), typeof(string), typeof(PlaceholderPasswordBox), new PropertyMetadata(""));
+        
+        static PlaceholderPasswordBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PlaceholderPasswordBox), new FrameworkPropertyMetadata(typeof(PlaceholderPasswordBox)));
+        }
+
+        public string PlaceholderText
+        {
+            get { return (string)GetValue(PlaceholderTextProperty); }
+            set { SetValue(PlaceholderTextProperty, value); }
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            if (GetTemplateChild("PART_PasswordBox") is PasswordBox pwd)
+            {
+                pwd.PasswordChanged += Pwd_PasswordChanged;
+            }
+        }
+
+        private void Pwd_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is PasswordBox pwd)
+            {
+                SetValue(TextProperty, pwd.Password);
+            }
+        }
+
+        private void CustomPasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (e.OriginalSource == this)
+            {
+                PasswordBox passwordBox = GetTemplateChild("PART_PasswordBox") as PasswordBox;
+                if (passwordBox != null)
+                {
+                    passwordBox.Focus();
+                }
+            }
+        }
+    }
+}
