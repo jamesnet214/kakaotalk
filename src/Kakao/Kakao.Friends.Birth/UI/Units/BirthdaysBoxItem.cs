a031555330b1c91230afb21a1c33660952d27fc7
JAEWUNG LEE <james@jamesnet.dev>
2023-03-25 17:56:25 +0900

    Updates

---
+++ src/Kakao/Kakao.Friends.Birth/UI/Units/BirthdaysBoxItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kakao.Friends.Birth.UI.Units
+{
+    public class BirthdaysBoxItem : ListBoxItem
+    {
+        static BirthdaysBoxItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(BirthdaysBoxItem), new FrameworkPropertyMetadata(typeof(BirthdaysBoxItem)));
+        }
+    }
+}
