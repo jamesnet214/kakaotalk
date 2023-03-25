392e6ed267bdeb82db83e2ad3f6b1103fd518b44
JAEWUNG LEE <james@jamesnet.dev>
2023-03-25 14:47:44 +0900

    Updates

--- src/Kakao/Kakao.Forms/UI/Views/KakaoWindow.cs
+++ src/Kakao/Kakao.Forms/UI/Views/KakaoWindow.cs
@@ -9,5 +9,11 @@ namespace Kakao.Forms.UI.Views
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(KakaoWindow), new FrameworkPropertyMetadata(typeof(KakaoWindow)));
         }
+
+        public KakaoWindow()
+        {
+            //WindowStyle = WindowStyle.None;
+            //AllowsTransparency = true; 
+        }
     }
 }
