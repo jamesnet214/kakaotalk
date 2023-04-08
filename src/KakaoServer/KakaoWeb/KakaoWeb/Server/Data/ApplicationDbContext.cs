5dd97ea80cb9e9d2e50f47d46d6ec6d842dc65e1
JAEWUNG LEE <james@jamesnet.dev>
2023-04-08 14:09:41 +0900

    Updates

---
+++ src/KakaoServer/KakaoWeb/KakaoWeb/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using KakaoWeb.Server.Models;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace KakaoWeb.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
