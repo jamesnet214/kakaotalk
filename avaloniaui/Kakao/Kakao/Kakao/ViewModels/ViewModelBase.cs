467f0a01bff00e92426d5c2594e6ed5e278fbb16
JAEWUNG LEE <james@jamesnet.dev>
2023-09-30 16:52:03 +0900

    Updates

---
+++ avaloniaui/Kakao/Kakao/Kakao/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Kakao.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ avaloniaui/Kakao/Kakao/Kakao/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Kakao.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
